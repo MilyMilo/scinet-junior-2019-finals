bf6777ff979f46a2cae5aa14b5539510c71bb481
Milosz Skaza <milosz.skaza@gmail.com>
2019-09-26 19:40:41 +0200

    finals-3: fixes

--- finals-3/machine/web/panel/login/authorize.php
+++ finals-3/machine/web/panel/login/authorize.php
@@ -5,12 +5,12 @@
 
         if($id == '54178912alice' && $pass == '0935612771342swordknight'){
             setcookie('logged_in', '54178912alice', time()+(60*5), '/');
-            header("Location: ../3315f43c-dc9b-11e9-8a34-2a2ae2dbcce4");
+            header("Location: /3315f43c-dc9b-11e9-8a34-2a2ae2dbcce4");
         }else if($id == '6128401hearthcliff' && $pass == 'bLOODkNIGHT0913administrator'){
             setcookie('logged_in', '6128401hearthcliff', time()+(60*15), '/');
-            header("Location: ../404.php");
+            header("Location: /404.php");
         }else{
-            header("Location: ../bad.php");
+            header("Location: /bad.php");
         }
     }
 
